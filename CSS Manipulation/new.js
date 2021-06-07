681849f4c88fda178205795f02d98b9d5be343ad
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-06-07 21:04:35 +0600

    Add files via upload

---
+++ CSS Manipulation/new.js
@@ -0,0 +1,15 @@
+//changing the existing CSS
+$("h1").css("color","blue");
+
+//Adding a new CSS
+$("h1").css("font-size","300%");
+$("h1").css("font-style", "italic");
+
+//all css in one function(Here we use json)
+$("h1").css({"background-color": 'red','width':'400px', 'padding': '20px',"border-radius": "20%"});
+
+//Adding class in an element
+$("p").addClass("styleByJs styleByJs1");
+
+//Rrmoving class of an specific element
+$("p").removeClass("styleByJs styleByJs1");
