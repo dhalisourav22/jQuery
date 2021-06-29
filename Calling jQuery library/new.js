91cfdcf56d822c890118470f56a25f717f92a6ec
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-06-29 10:30:08 +0600

    Update new.js

--- Calling jQuery library/new.js
+++ Calling jQuery library/new.js
@@ -1,5 +1,5 @@
-//Here the $ sign mean the document.querySelector
-//here text mean innerHTML or innerText
-//Here we change the text of html that's why we use the text function
+//Here the $ sign mean the document.querySelector.
+//here text mean innerHTML or innerText.
+//Here we change the text of html that's why we use the text function.
 
 $("h1").text("This is jQuery..!");
