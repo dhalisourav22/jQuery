56ba39f9b0640075dbd5360ebf35a6ac8d699fdc
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-07-01 11:21:48 +0600

    Update new.js

--- Password Checking/new.js
+++ Password Checking/new.js
@@ -1,15 +1,15 @@
-$("button").click(function(){
+$("button").click(function(){                   //This checking will be happen, when we are clicked the logIn button.
     let password1 = $("#pass1").val();
     let password2 = $("#pass2").val();
-    if(password1 != "" && password2 != ""){
-        if(password1 == password2){
+    if(password1 != "" && password2 != "")      //If any password of there is not empty or null.
+        if(password1 == password2){             //If password are matched.
             alert("Succesfully Login...!");
         }
-        else{
+        else{                                   //If password are not matched.
             alert("Password Mismatch...!");
         }
     }
-    else{
+    else{                                       //If any of those password is empaty or null.
         alert("Please Enter Password...!")
     }
 });
