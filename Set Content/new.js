cd989b52fec67622a49ccf1f770b0afdb62ec001
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-07-02 14:36:58 +0600

    Update new.js

--- Set Content/new.js
+++ Set Content/new.js
@@ -14,6 +14,25 @@
 
 //val()  -- This is changed the text field valur.
 
+// $("button").click(()=>{
+//     $("input").val("Sourav Dhali");
+// });
+
+
+//Using callback function in text();
+
+// $("button").click(()=>{
+//     $("p").text(function(index,oldContent){  //Here the first parameter means the index number and the second parameter means the old value of the parageraph.
+//         return "NEW : This is new text. OLD : "+oldContent+" INDEX : "+index;
+//     });
+// });
+
+
+//Using callback function in html();
+
 $("button").click(()=>{
-    $("input").val("Sourav Dhali");
+    $("p").html(function(i,old){  //Here the first parameter means the index number and the second parameter means the old value of the parageraph.
+        return "NEW : This is <strong>new text</strong>. OLD : "+old+" INDEX : "+i;
+    });
 });
+
