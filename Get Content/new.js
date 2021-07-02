c64a80249aa1e9144be55e7807a19e9cf3523196
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-07-02 14:06:42 +0600

    Add files via upload

---
+++ Get Content/new.js
@@ -0,0 +1,19 @@
+//text(). -- This is only show the plane text not the html tag.
+
+// $("button").click(()=>{
+//     alert($("p").text());
+// });
+
+
+//html(). -- This is show not only the plane text but also the html tag.
+
+// $("button").click(()=>{
+//     alert($("p").html());
+// });
+
+
+//val().  -- That's how we show the text field value.
+
+$("button").click(()=>{
+    alert($("input").val());
+});
