c0685669e1a0cdcba7678af02e8cea7d1a71aaf3
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-07-02 14:16:48 +0600

    Add files via upload

---
+++ Set Content/new.js
@@ -0,0 +1,19 @@
+//text()  -- This is set only plane text.
+
+// $("button").click(()=>{
+//     $("p").text("This is changed..!");
+// });
+
+
+//html()  -- This is set not only plane text but also this is set the text with html.
+
+// $("button").click(()=>{
+//     $("p").html("This is <strong>changed</strong>..!");
+// });
+
+
+//val()  -- This is changed the text field valur.
+
+$("button").click(()=>{
+    $("input").val("Sourav Dhali");
+});
