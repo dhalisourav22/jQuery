f5da5c7dc4d2b115692f733f85d76d18c6869abd
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-07-06 01:04:23 +0600

    Add files via upload

---
+++ Calculator/new.js
@@ -0,0 +1,22 @@
+
+let textArea = $("#result");
+function insertDigit(digit){
+    let currentDigit = textArea.val();   //This is return a string type value.
+    textArea.val(currentDigit + digit);   
+}
+
+function clearTextArea(){
+    textArea.val("");  //Setting value empty by empty string.
+}
+
+function calculate(){
+    let result = eval(textArea.val());   //eval is a function which is evaluate everything which is under it.
+    textArea.val(result);
+}
+
+function deleteDigit(){
+    let presentValue = textArea.val();
+    if(presentValue!=""){
+        textArea.val(presentValue.slice(0,-1));
+    }
+}
