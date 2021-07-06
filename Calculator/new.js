b082b77fb80163891298b0e2462b5c2e849e48ea
Sourav Dhali <70832555+dhalisourav22@users.noreply.github.com>
2021-07-06 11:06:39 +0600

    Update new.js

--- Calculator/new.js
+++ Calculator/new.js
@@ -1,22 +1,26 @@
 
-let textArea = $("#result");
-function insertDigit(digit){
-    let currentDigit = textArea.val();   //This is return a string type value.
-    textArea.val(currentDigit + digit);   
-}
+$(document).ready(()=>{
 
-function clearTextArea(){
-    textArea.val("");  //Setting value empty by empty string.
-}
+    let textArea = $("#result");
+    function insertDigit(digit){
+        let currentDigit = textArea.val();   //This is return a string type value.
+        textArea.val(currentDigit + digit);   
+    }   
 
-function calculate(){
-    let result = eval(textArea.val());   //eval() is a function which is evaluate everything which is under it.
-    textArea.val(result);
-}
-
-function deleteDigit(){
-    let presentValue = textArea.val();
-    if(presentValue!=""){
-        textArea.val(presentValue.slice(0,-1));
+    function clearTextArea(){
+        textArea.val("");  //Setting value empty by empty string.
     }
-}
+
+    function calculate(){
+        let result = eval(textArea.val());   //eval() is a function which is evaluate everything which is under it.
+        textArea.val(result);
+    }
+
+    function deleteDigit(){
+        let presentValue = textArea.val();
+        if(presentValue!=""){
+            textArea.val(presentValue.slice(0,-1));
+        }
+    }
+
+});
